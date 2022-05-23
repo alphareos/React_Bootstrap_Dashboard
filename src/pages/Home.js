54e8aef6fbe292813ea75d9510482d2b82bcbcb4
alphareos <muleta822@gmai.com>
2022-05-23 16:55:09 -0400

    customized sidebar and Navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,37 @@
+import React from "react"
 import Header from '../components/Header'
-import Meta from '../components/Meta'
 
 const Home = () => {
   // page content
-  const pageTitle = 'Home'
-  const pageDescription = 'welcome to react bootstrap template'
+  
 
   return (
-    <div>
-      <Meta title={pageTitle}/>
-      <Header head={pageTitle} description={pageDescription} />
+  
+    <div class="pcoded-main-container">
+    <div class="pcoded-content">
+        {/* <!-- [ breadcrumb ] start --> */}
+        <div class="page-header">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <div class="page-header-title">
+                            <h5 class="m-b-10">Dashboard Analytics</h5>
+                        </div>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a></li>
+                            <li class="breadcrumb-item"><a href="#!">Dashboard Analytics</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <!-- [ breadcrumb ] end --> */}
+        {/* <!-- [ Main Content ] start --> */}
+      
+        {/* <!-- [ Main Content ] end --> */}
     </div>
+</div>
+
   )
 }
 
